ce78749d112a467a0edd50f46f0ab80f62ba84e5
CaireB <c281135@dac.unicamp.br>
2025-09-03 16:26:03 -0300

    sdasdada

--- src/jogo/Personagem.java
+++ src/jogo/Personagem.java
@@ -1,9 +1,9 @@
 package jogo;
 // Criacao da classe abstrata para personagens
 public abstract class Personagem {
-    public String nome;
-    public int pontos_de_vida;
-    public int forca;
+    private String nome;
+    private int pontos_de_vida;
+    private int forca;
 
     public Personagem(String nome, int pontos_de_vida, int forca){
         this.nome = nome;
@@ -16,6 +16,23 @@ public abstract class Personagem {
 
 
     }
+    public String getNome(){
+        return this.nome;
+    }
+    public int getPontos_de_vida(){ 
+        return this.pontos_de_vida;
+    }
+    public int getForca(){
+        return this.forca;
+    }
+
+    public int setPontos_de_vida(int pontos_de_vida){
+        this.pontos_de_vida = pontos_de_vida;
+        return this.pontos_de_vida;
+    }
+    public int setForca(int forca){
+        return this.forca;
+    }
 
     public void Exibir_Status(){
         System.out.printf("Nome: %s\nVida: %d\nForça: %d\n", this.nome, this.pontos_de_vida, this.forca);
@@ -23,7 +40,6 @@ public abstract class Personagem {
 
 
     public abstract void atacar(Personagem p);
-    public abstract void usarHabilidadeEspecial();
 
 
 
